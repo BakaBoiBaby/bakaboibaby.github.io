3fe622d2a40ba6732eb0d89496b8bae0c53f01ad
BakaBoiBaby <61219887+BakaBoiBaby@users.noreply.github.com>
2020-03-01 19:38:33 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,27 +1,23 @@
+//Global Variables
 var hour, min, sec, todaysDate;
-//const destinationDate = new Date("March 20, 2020").getTime();
-const destinationDate = new Date("February 29, 2020").getTime();
+const destinationDate = new Date("March 20, 2020").getTime();
 
+//Auto Runs Code
 (function main(){
     countdownStart();
 })();
 
 function countdownStart(){
 
+    //Gather Time Variables
 	  todaysDate = new Date();
-    hour = todaysDate.getHours();
-    hour = formatTime(hour);
-    hour = formatTwoDigits(hour);
-    min = todaysDate.getMinutes();
-    min = formatTwoDigits(min);
-    sec = todaysDate.getSeconds();
-    sec = formatTwoDigits(sec);
 
     let presentTime = todaysDate.getTime();
     let timeBefore = destinationDate - presentTime;
     let timeAfter =  presentTime - destinationDate;
     setTimeout(countdownStart, 1000);
 
+    //After the designated date has passed
     if(timeBefore < 0){
       document.getElementById("timetext").innerHTML = "Days has passed";
     	document.getElementById("daysLeft").innerHTML = Math.floor(timeAfter/(1000*60*60*24));
@@ -29,6 +25,7 @@ function countdownStart(){
     	document.getElementById("minLeft").innerHTML = Math.floor(timeAfter % (1000*60*60) / (1000*60));
     	document.getElementById("secLeft").innerHTML = Math.floor(timeAfter % (1000*60) / 1000);
     }
+    //Before the designated date has passed
     else{
       document.getElementById("timetext").innerHTML = "Days before the day";
     	document.getElementById("daysLeft").innerHTML = Math.floor(timeBefore/(1000*60*60*24));
@@ -36,16 +33,4 @@ function countdownStart(){
     	document.getElementById("minLeft").innerHTML = Math.floor(timeBefore % (1000*60*60) / (1000*60));
     	document.getElementById("secLeft").innerHTML = Math.floor(timeBefore % (1000*60) / 1000);
     }
-}
-
-function formatTime(time){
-    time = ((time + 11) % 12 + 1);
-    return time;
-}
-
-function formatTwoDigits(time){
-    if(time < 10){
-        time = "0" + time;
-    }
-    return time;
 }
